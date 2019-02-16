5381c2f2b15e06ec1ae2deb6065559c4131d43ff
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-16 05:11:16 -0200

    Preparando componentes para usar em rotas

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
 import React, { Component } from 'react';
-import Todos from './Todos';
-import AddTodo from './AddTodo';
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './components/Home';
 
 class App extends Component {
-  state = {
-    todos: [
-      {id: 1, content: 'Comprar leite'},
-      {id: 2, content: 'Aprender Redux'}
-    ]
-  }
-
-  addTodo = todo => {
-    todo = {
-      id: Math.random() * 10,
-      content: todo
-    }
-    this.setState({
-      todos: [...this.state.todos, todo]
-    })
-  }
-
-  deleteTodo = id => {
-    const todos = this.state.todos.filter(todo => {
-      return todo.id !== id;
-    });
-
-    this.setState({
-      todos
-    })
-  }
-
   render() {
-    return(
-      <div className="todo-app container">
-        <h1 className="center blue-text">Tarefas</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
-        <AddTodo addTodo={this.addTodo} />
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route path="/" component={Home} />
       </div>
-    );
+      </BrowserRouter>
+    )
   }
 }
 
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam orci tellus, tincidunt ac risus nec, dictum consectetur mauris.
+        Phasellus rhoncus est sit amet vulputate gravida. Integer sed erat justo. Donec nec tortor id erat tempus imperdiet.
+        Aliquam sapien elit, lobortis sit amet egestas quis, tincidunt vitae tortor.
+        Curabitur convallis pharetra malesuada. Nunc sed efficitur dolor. Morbi suscipit urna est, a tempor enim suscipit ornare.</p>
+    </div>
+  )
+}
+
+export default Contact;
