6fed44a1b1da8ddf0593b8fb8a555c392f37e4af
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-16 05:21:10 -0200

    Prevenir ação padrão da ancora usando Link do react-router-dom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link, Navlink} from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
@@ -6,9 +7,9 @@ export default class Navbar extends Component {
       <nav className="nav-wrapper red darken-3">
         <a href="/" className="brand-logo">Poke'Times</a>
         <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
     )
