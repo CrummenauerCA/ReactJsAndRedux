4e0a97253ef3da466147c613dd8e02339f4db801
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-17 00:50:16 -0300

    Using data from an api with axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam orci tellus, tincidunt ac risus nec, dictum consectetur mauris.
-        Phasellus rhoncus est sit amet vulputate gravida. Integer sed erat justo. Donec nec tortor id erat tempus imperdiet.
-        Aliquam sapien elit, lobortis sit amet egestas quis, tincidunt vitae tortor.
+export default class Home extends Component {
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+      console.log(res);
+    }).catch();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam orci tellus, tincidunt ac risus nec, dictum consectetur mauris.
+          Phasellus rhoncus est sit amet vulputate gravida. Integer sed erat justo. Donec nec tortor id erat tempus imperdiet.
+          Aliquam sapien elit, lobortis sit amet egestas quis, tincidunt vitae tortor.
         Curabitur convallis pharetra malesuada. Nunc sed efficitur dolor. Morbi suscipit urna est, a tempor enim suscipit ornare.</p>
-    </div>
-  )
+      </div>
+    )
+  }
 }
-
-export default Home;
