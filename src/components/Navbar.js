8082946bea7c81e884fab415656baa8dc212deb7
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-17 00:28:56 -0300

    Sobrecarregando elementos com withRouter do react-router-dom para permitir redirecionamento

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import React from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <nav className="nav-wrapper red darken-3">
+const Navbar = (props) => {
+  console.log(props);
+  setTimeout(() => {
+    props.history.push('/about');
+  }, 3000);
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
         <a href="/" className="brand-logo">Poke'Times</a>
         <ul className="right">
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
-      </nav>
-    )
-  }
+      </div>
+    </nav>
+  )
 }
+
+export default withRouter(Navbar);
